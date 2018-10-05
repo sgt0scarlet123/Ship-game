c0a0ff4d5203df3cdd9001d77f7752a1b2d283e6
rikard.hjulstrom <rikard.hjulstrom@elev.ga.lbs.se>
2018-10-05 15:51:36 +0200

    Added Movments n Sprite

---
+++ Assets/Movements.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movements : MonoBehaviour
+{
+
+    public Transform forward;
+    public bool start;
+    public Transform Left;
+    public Transform Right;
+    public float Speed;
+
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+       bool start = false;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            start = true; 
+            
+        }
+        
+        if (start == true)
+        {
+            Speed = 0.05f;
+            transform.Translate(Speed, 0f, 0f, Space.Self);
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            Speed = (Speed / 5);
+            transform.Translate(Speed, 0f, 0f, Space.Self);
+        }
+        
+
+        if (Input.GetKey(KeyCode.A)) 
+        {
+            transform.Rotate(0f, -1f, 0f, Space.Self);
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0f, 2f, 0f, Space.Self);
+
+        }
+
+      
+
+
+
+
+    }
+}
