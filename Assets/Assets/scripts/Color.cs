bcbe3775d22a4be3051d29d4d8352bb008161277
rikard.hjulstrom <rikard.hjulstrom@elev.ga.lbs.se>
2018-10-07 12:47:11 +0200

    fixed scripts

--- Assets/Assets/scripts/Color.cs
+++ Assets/Assets/scripts/Color.cs
@@ -3,23 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Color : MonoBehaviour {
-    public Animator Left;
-    public Animator Right;
+    public Animator Left1;
+    public Animator Right1;
     public Animator Standard;
     public bool standard;
     public bool left;
     public bool right;
-    public float countdown= 10.0f;
+    public float countdown=2;
+    
     // Use this for initialization
     void Start()
     {
         Standard = GetComponent<Animator>();
-        Left = GetComponent<Animator>();
-        Right = GetComponent<Animator>();
+        Left1 = GetComponent<Animator>();
+        Right1 = GetComponent<Animator>();
         standard = false;
         right = false;
         left = false;
-        countdown = 10.0f;
+      
     }
 
 
@@ -36,35 +37,57 @@ public class Color : MonoBehaviour {
         {
             Standard.Play("New Animation");
         }
+      if (Input.GetKey(KeyCode.A))
+        {
+            countdown -= Time.deltaTime;
+        }
       if (Input.GetKeyDown(KeyCode.A))
         {
-            Left.Play("Left");
-            
-            
-            {while (countdown > 0.0f) ;
-                countdown -= (Time.deltaTime);
-            }
+            Left1.Play("Left");
         }
       if (countdown <= 0.0f)
         {
             left = true;
-            countdown = 10.0f;
         }
+        if (Input.GetKey(KeyCode.A) && (left == true)) 
+            {
 
-        if (Input.GetKey(KeyCode.A)&(left = true)) 
-        {
-            
-            Left.Play("Left2");
+            Left1.Play("Left2");
+
         }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            left = false;
+            countdown = 2;
+        }
+      
+
+        
        if (Input.GetKey(KeyCode.D))
         {
-            right = true;
+            countdown -= Time.deltaTime;
 
         }
-       if (right == true)
+       if (Input.GetKeyDown(KeyCode.D))
+           {
+            Right1.Play("Right");
+           }
+          if (countdown <= 0.0f)
         {
-            Right.Play("Right");
+            right = true;
         }
+
+        if (Input.GetKey(KeyCode.D) && (right == true)) 
+        {
+            Right1.Play("Right2");
+            
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            countdown = 2;
+            right = false;
+        }
+
        if (Input.GetKey(KeyCode.R))
         {
             standard = false;
