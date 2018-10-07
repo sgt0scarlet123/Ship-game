0a5d5c7884fede4ca01db26ba1fa749c4a19caba
rikard.hjulstrom <rikard.hjulstrom@elev.ga.lbs.se>
2018-10-07 10:59:26 +0200

    Revert "safe save"
    This reverts commit cb93336eb5b606f250fa43f967343f070efc0c6f.

--- Assets/Movements.cs
+++ Assets/Movements.cs
@@ -6,30 +6,26 @@ public class Movements : MonoBehaviour
 {
 
     public Transform forward;
-    public bool start;
+    public bool start=false;
     public Transform Left;
     public Transform Right;
     public float Speed;
-    public bool left;
-    public bool right;
-    public float left1;
-    public float right1;
-    public float SpeedRandomiser;
+
+
+
+
+
     // Use this for initialization
     void Start()
     {
-        start = false;
-        SpeedRandomiser = Random.Range(0.05f, 0.075f);
+       
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        left = false;
-        right = false;
-
         if (Input.GetKeyDown(KeyCode.T))
         {
             start = true; 
@@ -39,53 +35,36 @@ public class Movements : MonoBehaviour
         if (start == true)
         {
             transform.Translate(Speed, 0f, 0f, Space.Self);
-            Speed = SpeedRandomiser;
+            Speed = 0.05f;
 
         }
         else Speed=0;
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            Speed = (Speed + Speed /2);
-            transform.Translate(Speed, 0f, 0f, Space.Self);
-        }
 
         if (Input.GetKey(KeyCode.S))
         {
             Speed = (Speed / 5f);
             transform.Translate(Speed, 0f, 0f, Space.Self);
         }
+        
 
-
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A)) 
         {
-            left = true;
+            transform.Rotate(0f, -1f, 0f, Space.Self);
 
         }
-        if (left == true) {
-
-            transform.Rotate (0f, left1, 0f, Space.Self); 
-        }
-        
-
-        
 
         if (Input.GetKey(KeyCode.D))
         {
-            right = true;
-        }
-        if (right == true)
-        {
-            transform.Rotate(0f, right1, 0f, Space.Self);
-        }
-       
+            transform.Rotate(0f, 2f, 0f, Space.Self);
 
+        }
 
         if (Input.GetKey(KeyCode.R))
         {
             start = false;
-        }
 
+        }
       
         
 
