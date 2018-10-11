68642513465ad817e7465b369b82bc5329e3c822
rikard.hjulstrom <rikard.hjulstrom@elev.ga.lbs.se>
2018-10-11 14:09:52 +0200

    Fixed Everything Again

--- Assets/Assets/scripts/Timer.cs
+++ Assets/Assets/scripts/Timer.cs
@@ -2,20 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Timer : MonoBehaviour {
+public class Timer : MonoBehaviour
+{
 
-    public float time;
+    public float Sec;
     public bool timeonoff;
     public int PlusOne;
+    public int timer;
+    public string Timer_Min_Sec = "zero";
+    public int Min;
+    public int Minutes;
 
-    
 
-
-	// Use this for initialization
-	void Start () {
-        time = 0;
+    // Use this for initialization
+    void Start()
+    {
+        Sec = 0;
         PlusOne = 1;
-	}
+        timer = 0;
+        Min = 0;
+        Minutes = 0;
+        Timer_Min_Sec = "Timer";
+                               
+        
+                
+    }
 
     // Update is called once per frame
     void Update()
@@ -33,23 +44,40 @@ public class Timer : MonoBehaviour {
     }
     void FixedUpdate()
     {
-        
+
         if (timeonoff == true)
         {
-            time += PlusOne * Time.deltaTime;
-            Debug.Log(time);
+            Sec += PlusOne * Time.deltaTime;
+            
         }
-        
-
+        timer = Mathf.RoundToInt(Sec);
+        Minutes = Mathf.RoundToInt(Min);
 
         if (Input.GetKey(KeyCode.R))
         {
             timeonoff = false;
 
         }
-     
-        
+        if (Sec >= 60f)
+        {
+            Min = Min + 1;
+            Sec = 0;
+        }
+        Debug.LogFormat("{0},,{2},{3}:",Timer_Min_Sec,"",Minutes,timer,"");
     }
+        
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
